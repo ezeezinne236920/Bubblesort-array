06f4ae78f4e64198611dc2f5dade4e16d751a836
ezeezinne236920 <ezeezinne42@gmail.com>
2023-08-29 10:23:21 +0100

    loop.js is modified

--- Loop.js
+++ Loop.js
@@ -1,27 +1,12 @@
-// getting the element...
-const innerloop = document.getElementById("number");
-// creating a bubble sort array function...
+const number = document.getElementById("number");
+let numbers = [5, 2, 0, -3, 4, 5, 2, 10, 40, 1, 3, 4];
 
-const arrangement = (list) => {
-  for (let i = 0; i < list.length; i++) {
-    // Checking if the i elements are already in place
+let spacedNumbers = [];
 
-    for (let j = 0; j < list.length - i - 1; j++) {
-      // Checking if the item at present iteration is greater than the next iteration
-
-      if (list[j] > list[j + 1]) {
-        // swapping them is the conditions are true
-        let temp = list[j];
-        list[j] = list[j + 1];
-        list[j + 1] = temp;
-      }
-    }
+for (let i = -3; i <= 40; i++) {
+  if (numbers.includes(i)) {
+    spacedNumbers.push(i);
   }
-  innerloop.innerHTML = `the sorted array is ${list}`;
-};
+}
 
-// This is our unsorted array
-const list = [5, 2, 0, -3, 4, 5, 2, 10, 40, 1, 3, 4];
-
-// Now pass this array to the bblSort() function
-arrangement(list);
+number.innerHTML = `the sorted numbers are: ${spacedNumbers.join(" ")}`;
