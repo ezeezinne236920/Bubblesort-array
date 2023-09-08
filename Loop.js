c2416b9750f57457b14f7bfa6b956c650265df0b
Eze Ezinne Leticia <ezeezinne42@gmail.com>
2023-09-08 18:50:55 +0100

    sortedARRay

--- Loop.js
+++ Loop.js
@@ -1,12 +1,35 @@
-const number = document.getElementById("number");
-let numbers = [5, 2, 0, -3, 4, 5, 2, 10, 40, 1, 3, 4];
+const number = document.getElementById('number')
+let numbers = [5, 2, 0, -3, 4, 5, 2, 10, 40, 1, 3, 4]
 
-let spacedNumbers = [];
+const spacedNumbers = []
 
-for (let i = -3; i <= 40; i++) {
-  if (numbers.includes(i)) {
-    spacedNumbers.push(i);
+let sortNumbers = (array) => {
+  let position = 0,
+    max = array[0],
+    min = array[0]
+  // getting the maximum number
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i]
+    }
   }
+  //  creating a loop that stores the minimum value of zero to n position
+  for (let j = 0; j < array.length; j++) {
+    // finding the minimum number
+    for (let k = 0; k < array.length; k++) {
+      if (array[k] != null) {
+        if (min > array[k]) {
+          min = array[k]
+          position = k
+        }
+      }
+    }
+    spacedNumbers[j] = min
+    array[position] = null
+    min = max
+  }
+  return spacedNumbers
 }
+const sorterdNumberList = sortNumbers(numbers).join(' ')
 
-number.innerHTML = `the sorted numbers are: ${spacedNumbers.join(" ")}`;
+number.innerHTML = `the sorted numbers are: ${sorterdNumberList}`
